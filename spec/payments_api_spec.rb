b498d71e2693a162f6b0d9734a70d326a9e11d0e
Ismael Celis <ismaelct@gmail.com>
2016-07-31 19:14:57 +0100

    Test PaymentsApi#payments_post

--- spec/payments_api_spec.rb
+++ spec/payments_api_spec.rb
@@ -40,5 +40,42 @@ describe Khipu::PaymentsApi do
       expect(subject.payments_get("ABC")).to eq response_object
     end
   end
+
+  describe "#payments_post(subject, currency, amount, opts)" do
+    let(:response_object) {
+      Khipu::PaymentsCreateResponse.new(
+        payment_id: 123
+      )
+    }
+
+    it "uses api_client to process request and wrap response" do
+      expect(api_client).to receive(:call_api) do |http_method, path, opts|
+        expect(http_method).to eq :POST
+        expect(path).to eq "/payments"
+        expect(opts[:header_params]["Accept"]).to eq "application/json"
+        expect(opts[:header_params]["Content-Type"]).to eq "application/x-www-form-urlencoded"
+        expect(opts[:query_params]).to eq({})
+        expect(opts[:form_params]["subject"]).to eq "a payment"
+        expect(opts[:form_params]["currency"]).to eq "CLP"
+        expect(opts[:form_params]["amount"]).to eq 1000
+        expect(opts[:form_params]["send_email"]).to eq true
+        expect(opts[:form_params]["payer_name"]).to eq "John Payer"
+        expect(opts[:form_params]["payer_email"]).to eq "payer@email.com"
+        expect(opts[:auth_names]).to eq ["khipu"]
+        expect(opts[:return_type]).to eq "PaymentsCreateResponse"
+      end.and_return response_object
+
+      resp = subject.payments_post(
+        "a payment",
+        "CLP",
+        1000,
+        send_email: true,
+        payer_name: "John Payer",
+        payer_email: "payer@email.com"
+      )
+
+      expect(resp).to eq response_object
+    end
+  end
 end
 
