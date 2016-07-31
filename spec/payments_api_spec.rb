77db56a964e8d267363738845819ea16f9ba9176
Ismael Celis <ismaelct@gmail.com>
2016-07-31 19:49:05 +0100

    Test PaymentsApi#payments_id_get

--- spec/payments_api_spec.rb
+++ spec/payments_api_spec.rb
@@ -77,5 +77,30 @@ describe Khipu::PaymentsApi do
       expect(resp).to eq response_object
     end
   end
+
+  describe "#payments_id_get(id, opts)" do
+    let(:response_object) {
+      Khipu::PaymentsResponse.new(
+       payment_id: "123",
+       payment_url: "http://host.com/a/b",
+      )
+    }
+
+    it "uses api_client to process request and wrap response" do
+      expect(api_client).to receive(:call_api) do |http_method, path, opts|
+        expect(http_method).to eq :GET
+        expect(path).to eq "/payments/123"
+        expect(opts[:header_params]["Accept"]).to eq "application/json"
+        expect(opts[:header_params]["Content-Type"]).to eq "application/x-www-form-urlencoded"
+        expect(opts[:query_params]).to eq({})
+        expect(opts[:form_params]).to eq({})
+        expect(opts[:body]).to be_nil
+        expect(opts[:auth_names]).to eq ["khipu"]
+        expect(opts[:return_type]).to eq "PaymentsResponse"
+      end.and_return response_object
+
+      expect(subject.payments_id_get(123)).to eq response_object
+    end
+  end
 end
 
